6542db0c1c370d24e46764faa9c5dc3e689c0a9e
Filip Halon <filu.halon@gmail.com>
2022-12-09 14:42:33 +0100

    List of videos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
-import React from 'react';
+// import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyCrcoazmYnnYs5ocFv3ktI0bebv2myv7os';
 
-//const API_KEY = 'AIzaSyDCPP6a2un11mqg5PoYqKPlOY_eEVl5rI8';
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
 
-const App = () => {
-    return <div>
-        <SearchBar />
-    </div>;
+//         this.state = { videos: [] };
+
+//         YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+//             this.setState({ videos });
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <SearchBar />
+//                 <VideoList videos={this.state.videos} />
+//             </div>
+//         );
+//     }
+// }
+
+function App () {
+    const [videos, setVideos] = useState([]);
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+        setVideos(videos);
+    });
+
+    return (
+        <div>
+            <SearchBar />
+            <VideoList videos={videos} />
+        </div>
+    );
 }
 
 const root = createRoot(document.querySelector('.container'));
